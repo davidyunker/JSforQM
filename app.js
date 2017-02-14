217abf06a3fa864c08af76529b82011605f26b3e
David Yunker <david.yunker@gmail.com>
2017-02-14 14:20:41 -0600

    got it working with multiple for loops

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 
 
 var button = document.getElementById("parseBtn");
-var preambleNow = document.getElementById('preamble').innerText.toLowerCase();
+var preambleNow = document.getElementById('preamble').innerText.toLowerCase().replace(/[^\w\s]|_/g, "");
 var tStartCounter = 0;
 var eEndCounter = 0;
 var bothCounter = 0;
-var otherWordsCounter = 0;
 button.addEventListener("click", parseText);
 
 
 function parseText () {
   console.log("this is the preamble", preambleNow)
-  console.log("This is where you would parse the text");
   var preambleArray = preambleNow.split(" ");
   console.log("this is the preamble now", preambleArray)
   console.log("here's the length of the preamble", preambleArray.length);
 
     for (var i = 0; i < preambleArray.length; i++)
 
+    if (preambleArray[i].startsWith("t")){
+        tStartCounter += 1;
+      }
 
-      // if (preambleArray[i].endsWith("e") || preambleArray[i].endsWith("e,")) {
-      //   eEndCounter += 1;
-      // }
 
-      // if (preambleArray[i].startsWith("t")){
-      //   tStartCounter += 1;
-      // }
+    for (var i = 0; i < preambleArray.length; i++)
+
+      if  (preambleArray[i].endsWith("e")) {
+        eEndCounter += 1;
+      }
+
+
+      for (var i = 0; i < preambleArray.length; i++)
+
 
       if (preambleArray[i].startsWith("t") && preambleArray[i].endsWith("e")) {
         bothCounter+= 1;
@@ -35,8 +39,10 @@ function parseText () {
 
 
 
-   // console.log(eEndCounter);
-   // console.log(tStartCounter);
+
+
+   console.log(eEndCounter);
+   console.log(tStartCounter);
    console.log(bothCounter);
   document.getElementById('endE').innerText = eEndCounter;
   document.getElementById('startT').innerText = tStartCounter;
